f81bce1c05d8685c37b384da1a18630cc508a9b5
vvkar <vv.karankevich@gmail.com>
2021-01-25 21:22:34 +0300

    second commit

---
+++ TMS.Net07.Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!!");
+            
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
